7ac8ca7087d9a105deac2722b16cd2f6a0e02fe4
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 06:52:52 +0700

    Add convert in JwtAuthConverter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootkeycloakintegration/config/JwtAuthConverter.java
+++ src/main/java/id/my/hendisantika/springbootkeycloakintegration/config/JwtAuthConverter.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.springbootkeycloakintegration.config;
 
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,18 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
     private String principleAttribute;
     @Value("${jwt.auth.converter.resource-id}")
     private String resourceId;
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                getPrincipleClaimName(jwt)
+        );
+    }
 }
