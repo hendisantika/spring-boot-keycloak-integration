a5092b7f9dfe8d88e63823d15111e70171de50cb
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 06:54:10 +0700

    Add extractResourceRoles in JwtAuthConverter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootkeycloakintegration/config/JwtAuthConverter.java
+++ src/main/java/id/my/hendisantika/springbootkeycloakintegration/config/JwtAuthConverter.java
@@ -2,8 +2,10 @@ package id.my.hendisantika.springbootkeycloakintegration.config;
 
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -11,6 +13,8 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -55,5 +59,26 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
             claimName = principleAttribute;
         }
         return jwt.getClaim(claimName);
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (jwt.getClaim("resource_access") == null) {
+            return Set.of();
+        }
+        resourceAccess = jwt.getClaim("resource_access");
+
+        if (resourceAccess.get(resourceId) == null) {
+            return Set.of();
+        }
+        resource = (Map<String, Object>) resourceAccess.get(resourceId);
+
+        resourceRoles = (Collection<String>) resource.get("roles");
+        return resourceRoles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }
