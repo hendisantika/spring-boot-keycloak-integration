1e54db1328766b98bddcc86d7472509149f462eb
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 06:47:02 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootkeycloakintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloakIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootkeycloakintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
