9ff9d9da75c1848bd454807022d7ebf1b9d92615
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 06:54:40 +0700

    Add DemoController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootkeycloakintegration/controller/DemoController.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.springbootkeycloakintegration.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-integration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/16/24
+ * Time: 06:54
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+}
